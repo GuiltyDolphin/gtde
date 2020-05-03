74ed8c08345515b415635cee4e066de1e3e5b4b3
Ben Moon <software@guiltydolphin.com>
2020-05-03 11:42:45 +0100

    Add initial code file

---
+++ org-gtd.el
@@ -0,0 +1,34 @@
+;;; org-gtd.el --- Tools for working with GTD and org-mode -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2020 Ben Moon
+;; Author: Ben Moon <software@guiltydolphin.com>
+;; URL: https://github.com/GuiltyDolphin/org-gtd
+;; Git-Repository: git://github.com/GuiltyDolphin/org-gtd.git
+;; Created: 2020-05-03
+;; Version: 0.0.0
+;; Keywords: outlines, tools
+;; Package-Requires: ((emacs "24"))
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+
+;; org-gtd provides tools for working with GTD workflows and org-mode.
+;;
+;; For more information see the README.
+
+;;; Code:
+
+(provide 'org-gtd)
+;;; org-gtd.el ends here
