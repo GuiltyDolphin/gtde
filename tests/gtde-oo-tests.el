640713abf9c9997c3fd11a4ee091082ea8457851
Ben Moon <software@guiltydolphin.com>
2021-04-28 17:19:03 +0100

    Add macro for splitting up tests for project types

--- tests/gtde-oo-tests.el
+++ tests/gtde-oo-tests.el
@@ -7,9 +7,9 @@
 (require 'gtde-org)
 
 
-(defun gtde-test--find-test-case-file (basename)
-  "Find the test case file path for BASENAME."
-  (format "tests/cases/%s" basename))
+(defun gtde-test--find-test-case-file (pt basename)
+  "Find the test case file path for BASENAME and the given project type PT."
+  (format "tests/cases/%s.%s" basename pt))
 
 (defun gtde--map-class-leaves (fun classes)
   "Map FUN over leaf classes of CLASSES.
@@ -61,6 +61,25 @@ PT is the project-type to create a file for."
   (let ((db (gtde--build-db-from-files project-type (list file))))
     (gtde--db-get-entry db id)))
 
+(defmacro gtde-test--test-each-project-type (basename pts ptvar doc &rest body)
+  "Define a test for each project type in PTS.
+
+BASENAME is the base name of the test.
+
+In each test, PTVAR holds the value of the current project type.
+
+DOC is the test documentation.
+BODY is the test body."
+  (declare (indent 3) (debug t))
+  (let ((pts (eval pts)))
+    (dolist (pt pts)
+      (let ((test-name (intern (format "gtde-oo-test:%s:%s" basename pt))))
+        ;;`(ert-deftest ,(make-symbol (format "gtde-oo-test:%s:%s" basename pt)) ()
+        (eval `(ert-deftest ,test-name ()
+                 ,doc
+                 (let ((,ptvar ',pt))
+                   ,@body)))))))
+
 
 ;;;;;;;;;;;;;;;;;
 ;;;;; Tests ;;;;;
@@ -77,55 +96,53 @@ PT is the project-type to create a file for."
   (let ((leaves-with-type-name (gtde--map-class-leaves (lambda (c) (cons c (gtde--oref-default-or-nil c type-name))) '(gtde--from-org))))
     (--each leaves-with-type-name (-let (((c . k) it)) (should-not (equal (cons c nil) (cons c k)))))))
 
-(ert-deftest gtde-oo-test:gtde--build-db-from-files ()
+(gtde-test--test-each-project-type build-db-from-files '(org json) pt
   "Tests for `gtde--build-db-from-files'."
-  (dolist (file (list "01-simple.org" "01-simple.json"))
-    (let* ((pt (if (equal (file-name-extension file) "json") 'json 'org))
-           (db (gtde--build-db-from-files pt (list (gtde-test--find-test-case-file file)))))
+  (let ((db (gtde--build-db-from-files pt (list (gtde-test--find-test-case-file pt "01-simple")))))
 
-      ;; correct IDs should be parsed into items
-      (should (equal (-sort #'string-lessp (hash-table-keys (oref db table)))
-                     '("02-project" "03-action-standalone" "04-action-with-project" "05-waiting-for-with-project")))
-      (let ((config (oref db global-config))
-            (project (gtde--db-get-entry db "02-project"))
-            (action-standalone (gtde--db-get-entry db "03-action-standalone"))
-            (action-with-project (gtde--db-get-entry db "04-action-with-project"))
-            (waiting-for-with-project (gtde--db-get-entry db "05-waiting-for-with-project")))
+    ;; correct IDs should be parsed into items
+    (should (equal (-sort #'string-lessp (hash-table-keys (oref db table)))
+                   '("02-project" "03-action-standalone" "04-action-with-project" "05-waiting-for-with-project")))
+    (let ((config (oref db global-config))
+          (project (gtde--db-get-entry db "02-project"))
+          (action-standalone (gtde--db-get-entry db "03-action-standalone"))
+          (action-with-project (gtde--db-get-entry db "04-action-with-project"))
+          (waiting-for-with-project (gtde--db-get-entry db "05-waiting-for-with-project")))
 
-        ;; testing config
-        (should (equal 'gtde--config (eieio-object-class config)))
-        (should (equal (list (gtde--project-status :display "ACTIVE" :is-active t)
-                             (gtde--project-status :display "COMPLETE" :is-active nil)
-                             (gtde--project-status :display "CANCELLED" :is-active nil))
-                       (oref config project-statuses)))
+      ;; testing config
+      (should (equal 'gtde--config (eieio-object-class config)))
+      (should (equal (list (gtde--project-status :display "ACTIVE" :is-active t)
+                           (gtde--project-status :display "COMPLETE" :is-active nil)
+                           (gtde--project-status :display "CANCELLED" :is-active nil))
+                     (oref config project-statuses)))
 
-        ;; testing project
-        (should (equal 'gtde--project (eieio-object-class project)))
-        (should (equal "02-project" (oref project id)))
-        (should (equal "Test project" (oref project title)))
-        (should (equal (gtde--project-status :display "COMPLETE" :is-active nil) (oref project status)))
+      ;; testing project
+      (should (equal 'gtde--project (eieio-object-class project)))
+      (should (equal "02-project" (oref project id)))
+      (should (equal "Test project" (oref project title)))
+      (should (equal (gtde--project-status :display "COMPLETE" :is-active nil) (oref project status)))
 
-        ;; testing action-standalone
-        (should (equal 'gtde--next-action (eieio-object-class action-standalone)))
-        (should (equal "03-action-standalone" (oref action-standalone id)))
-        (should (equal "A test standalone action" (oref action-standalone title)))
+      ;; testing action-standalone
+      (should (equal 'gtde--next-action (eieio-object-class action-standalone)))
+      (should (equal "03-action-standalone" (oref action-standalone id)))
+      (should (equal "A test standalone action" (oref action-standalone title)))
 
-        ;; testing action-with-project
-        (should (equal 'gtde--next-action (eieio-object-class action-with-project)))
-        (should (equal "04-action-with-project" (oref action-with-project id)))
-        (should (equal '("02-project") (oref action-with-project superior-projects)))
-        (should (equal "Action of \"a test project\"" (oref action-with-project title)))
-        (should (equal (gtde--some (list (gtde--context :name "test_context"))) (oref action-with-project context)))
+      ;; testing action-with-project
+      (should (equal 'gtde--next-action (eieio-object-class action-with-project)))
+      (should (equal "04-action-with-project" (oref action-with-project id)))
+      (should (equal '("02-project") (oref action-with-project superior-projects)))
+      (should (equal "Action of \"a test project\"" (oref action-with-project title)))
+      (should (equal (gtde--some (list (gtde--context :name "test_context"))) (oref action-with-project context)))
 
-        ;; testing waiting-for-with-project
-        (should (equal 'gtde--waiting-for (eieio-object-class waiting-for-with-project)))
-        (should (equal "05-waiting-for-with-project" (oref waiting-for-with-project id)))
-        (should (equal '("02-project") (oref waiting-for-with-project superior-projects)))
-        (should (equal "Waiting for of \"a test project\"" (oref waiting-for-with-project title))))))
+      ;; testing waiting-for-with-project
+      (should (equal 'gtde--waiting-for (eieio-object-class waiting-for-with-project)))
+      (should (equal "05-waiting-for-with-project" (oref waiting-for-with-project id)))
+      (should (equal '("02-project") (oref waiting-for-with-project superior-projects)))
+      (should (equal "Waiting for of \"a test project\"" (oref waiting-for-with-project title)))))
 
   ;; unsupported GTD type
   (should (equal "something_unsupported"
-                 (cdr (should-error (gtde--build-db-from-files 'org (list (gtde-test--find-test-case-file "02-bad.org"))) :type 'gtde--unsupported-gtd-type)))))
+                 (cdr (should-error (gtde--build-db-from-files pt (list (gtde-test--find-test-case-file pt "02-bad"))) :type 'gtde--unsupported-gtd-type)))))
 
 (ert-deftest gtde-oo-test:parse-from-raw ()
   "Tests for `gtde--parse-from-raw'."
@@ -134,13 +151,13 @@ PT is the project-type to create a file for."
     (should (equal (gtde--context :name "test") (gtde--parse-from-raw 'org #'gtde--context config1 "@test")))
     (should (equal (gtde--context :name "@test") (gtde--parse-from-raw 'org #'gtde--context config2 "@test")))))
 
-(ert-deftest gtde-oo-test:write-item-to-file ()
+(gtde-test--test-each-project-type write-item-to-file '(org json) pt
   "Tests for `gtde--write-item-to-file'."
   ;; all fields of projects and actions can be written
   (let ((example-action (gtde--next-action :title "Modified action title" :id "01-test-action"))
-        (example-project (gtde--project :title "Modified title" :id "01-test-project" :status (gtde--project-status :display "INACTIVE" :is-active nil))))
-    (let ((case-text "
-* Test config
+        (example-project (gtde--project :title "Modified title" :id "01-test-project" :status (gtde--project-status :display "INACTIVE" :is-active nil)))
+        (case-text (if (eq pt 'org)
+"* Test config
 :PROPERTIES:
 :GTDE_IS_CONFIG: t
 :GTDE_PROJECT_STATUSES: ACTIVE | INACTIVE
@@ -156,14 +173,8 @@ PT is the project-type to create a file for."
 :ID: 01-test-project
 :GTDE_TYPE: project
 :STATUS: ACTIVE
-:END:"))
-      (gtde-test--with-temp-file-for-project 'org "test-file" case-text fvar
-        (gtde--write-item-to-file 'org fvar example-project)
-        (gtde--write-item-to-file 'org fvar example-action)
-        (should (equal example-project (gtde-test--find-item-by-id-in-file 'org "01-test-project" fvar)))
-        (should (equal example-action (gtde-test--find-item-by-id-in-file 'org "01-test-action" fvar)))))
-    (let ((case-text "
-{
+:END:" (if (eq pt 'json)
+"{
   \"config\": {
       \"GTDE_IS_CONFIG\": true,
       \"GTDE_PROJECT_STATUSES\": \"ACTIVE | INACTIVE\",
@@ -180,12 +191,12 @@ PT is the project-type to create a file for."
       \"GTDE_TYPE\": \"project\",
       \"STATUS\": \"ACTIVE\"
   }
-}"))
-      (gtde-test--with-temp-file-for-project 'json "test-file" case-text fvar
-        (gtde--write-item-to-file 'json fvar example-project)
-        (gtde--write-item-to-file 'json fvar example-action)
-        (should (equal example-project (gtde-test--find-item-by-id-in-file 'json "01-test-project" fvar)))
-        (should (equal example-action (gtde-test--find-item-by-id-in-file 'json "01-test-action" fvar)))))))
+}"))))
+      (gtde-test--with-temp-file-for-project pt "test-file" case-text fvar
+        (gtde--write-item-to-file pt fvar example-project)
+        (gtde--write-item-to-file pt fvar example-action)
+        (should (equal example-project (gtde-test--find-item-by-id-in-file pt "01-test-project" fvar)))
+        (should (equal example-action (gtde-test--find-item-by-id-in-file pt "01-test-action" fvar))))))
 
 (ert-deftest gtde-oo-test:build-db-from-files:no-such-file ()
   "Testing `gtde--build-db-from-files' when passed nonexistent files."
@@ -195,13 +206,9 @@ PT is the project-type to create a file for."
       (delete-file fvar)
       (should-error (gtde--build-db-from-files pt (list fvar)) :type 'gtde--no-such-file))))
 
-(ert-deftest gtde-oo-test:unknown-project-status ()
+(gtde-test--test-each-project-type unknown-project-status '(org json) pt
   "An unknown project status is specified."
-  (should-error (gtde--build-db-from-files 'json (list (gtde-test--find-test-case-file "04-unknown-status.json"))) :type 'gtde--unknown-project-status))
-
-(ert-deftest gtde-oo-test:unknown-project-status-org ()
-  "An unknown project status is specified."
-  (should-error (gtde--build-db-from-files 'org (list (gtde-test--find-test-case-file "04-unknown-status.org"))) :type 'gtde--unknown-project-status))
+  (should-error (gtde--build-db-from-files pt (list (gtde-test--find-test-case-file pt "04-unknown-status"))) :type 'gtde--unknown-project-status))
 
 
 (provide 'gtde-oo-tests)
